9bb26aa623748d39b97ea0b2aa6acd7bd8f6691d
Prabhat-022 <lograjanand1250@gmail.com>
2025-01-30 00:16:53 +0530

    Re arrange file

--- server/index.js
+++ server/index.js
@@ -15,14 +15,11 @@ dotenv.config({
 
 const port = process.env.PORT || 6000;
 
-// Define __dirname for ES modules
-const _dirname = path.resolve();
-console.log('_dirname', _dirname);
 
 // For parsing application/json
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -32,16 +29,11 @@ app.use(cors({
     credentials: true,
 }));
 
-// Serve static files from the React app
-app.use(express.static(path.join(_dirname, "/client/dist")));
 // Create API
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/message", messageRoutes);
 
-// Handle all other routes by serving the index.html from the React app
-app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"))
-  })
+
 
 server.listen(port, () => {
     DatabaseConnection();
